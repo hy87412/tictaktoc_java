da9d32d10062f289b3fb4589787e941a1d16a39c
hy87412 <hy87412@naver.com>
2023-11-22 20:07:24 +0900

    catch bugs & ressetboard trans method

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,47 @@
 import java.util.Scanner;
 public class Main {
     //board
-    static int tttboard_size = 3;
+    static final int tttboard_size = 3;
     static int[][] tttboard = new int[3][3];
 
     public static void main(String[] args) {
-        //board reset
+        resettttboard();
+        while (true) {
+            printtttboard();
+            userinputandchange(1);
+            printtttboard();
+            userinputandchange(0);
+        }
+    }
+
+
+    static void resettttboard() {
         for (int row = 0; row <= tttboard_size - 1; row++) {
             for (int column = 0; column <= tttboard_size - 1; column++) {
                 tttboard[row][column] = 2;
             }
         }
-
-        inputuserpickandchange(1);
-        tttboardprint();
-        inputuserpickandchange(0);
-        tttboardprint();
-
     }
 
-
-    static void inputuserpickandchange(int usertype) {
+    static void userinputandchange(int usertype) {
         Scanner scan = new Scanner(System.in);
         int row, column;
         while (true) {
             row = scan.nextInt();
             column = scan.nextInt();
             if ((row >= 1 && row <= 3) && (column >= 1 && column <= 3)) {
-                if (usertype == 1) {
-                    tttboard[row - 1][column - 1] = 1;
+                if (2 == tttboard[row - 1][column - 1]) {
+                    if (usertype == 1) {
+                        tttboard[row - 1][column - 1] = 1;
+                    }
+                    else {
+                        tttboard[row - 1][column - 1] = 0;
+                    }
+                    break;
                 }
-                else {
-                    tttboard[row - 1][column - 1] = 0;
+                else if (2 != tttboard[row - 1][column - 1]) {
+                    System.out.println("빈칸에다 입력하세요");
                 }
-                break;
             }
             else {
                 System.out.println("행 열 꼴로 1에서 3사이에 정수를 입력하세요");
@@ -41,7 +49,7 @@ public class Main {
         }
     }
 
-    static void tttboardprint() {
+    static void printtttboard() {
         System.out.println("\t1\t2\t3");
         for (int row = 0; row <= tttboard_size - 1; row++) {
             System.out.print(row + 1);
@@ -58,5 +66,9 @@ public class Main {
             }
             System.out.println();
         }
+    }
+
+    static void tttgameplay() {
+
     }
 }
